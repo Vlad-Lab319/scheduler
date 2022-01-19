8645f892113276649c88efa510fcf476ef4bfc1c
vlad-lab319 <solo15mag@gmail.com>
2022-01-18 22:33:00 -0500

    Add DayListItem test and formatSpots function

--- src/components/DayListItem.jsx
+++ src/components/DayListItem.jsx
@@ -8,10 +8,23 @@ export default function DayListItem(props) {
     "day-list__item--selected": props.selected,
     "day-list__item--full": props.spots === 0
   });
+
+  // const spots = props.spots;
+
+  const formatSpots = (spots) => {
+    if (spots === 0) {
+      return `no spots remaining`;
+    }
+    if (spots === 1) {
+      return `${spots} spot remaining`; 
+    }
+    return `${spots} spots remaining`; 
+  };
+
   return (
     <li onClick={() => props.setDay(props.name)} className={dayClass}>
       <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light" >{props.spots} spots remaining</h3>
+      <h3 className="text--light" >{formatSpots(props.spots)}</h3>
     </li>
   );
 };
