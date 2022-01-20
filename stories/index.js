6402f6dfd76ba468f6498059ad892c192056d8b6
vlad-lab319 <solo15mag@gmail.com>
2022-01-20 00:24:32 -0500

    Add Error component and story for that

--- stories/index.js
+++ stories/index.js
@@ -16,6 +16,7 @@ import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
 import Status from "components/Appointment/Status";
+import Error from "components/Appointment/Error";
 
 // Test for button component
 storiesOf("Button", module)
@@ -184,5 +185,11 @@ storiesOf("InterviewerListItem", module)
     .add("Status", () => (
       <Status
         message='Deleting'
+      />
+    ))
+    .add("Error", () => (
+      <Error
+        message="Could not delete appointment."
+        onClose={action("onClose")}
       />
     ))
