5ec6db319bc319dbcbbfa42b8326e991d47b3a2c
vlad-lab319 <solo15mag@gmail.com>
2022-01-20 00:44:09 -0500

    Add Form component and stories for that

--- stories/index.js
+++ stories/index.js
@@ -17,6 +17,7 @@ import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
 import Status from "components/Appointment/Status";
 import Error from "components/Appointment/Error";
+import Form from "components/Appointment/Form";
 
 // Test for button component
 storiesOf("Button", module)
@@ -191,5 +192,21 @@ storiesOf("InterviewerListItem", module)
       <Error
         message="Could not delete appointment."
         onClose={action("onClose")}
+      />
+    ))
+    .add("Form Edit", () => (
+      <Form
+        student='Vlad'
+        interviewer={2}
+        interviewers={interviewers}
+        onSave={action("onSave")}
+        onCancel={action("onCancel")}
+      />
+    ))
+    .add("Form Create", () => (
+      <Form
+        interviewers={interviewers}
+        onSave={action("onSave")}
+        onCancel={action("onCancel")}
       />
     ))
