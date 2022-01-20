9a8057a90c6a9a84dfc05c7444bee2a16c16d291
vlad-lab319 <solo15mag@gmail.com>
2022-01-20 13:35:10 -0500

    Apointment component with mock data mounted to Application

--- src/components/Application.js
+++ src/components/Application.js
@@ -93,6 +93,10 @@ export default function Application(props) {
       </section>
       <section className="schedule">
         {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {appointments.map(appointment => 
+          <Appointment key={appointment.id} {...appointment} />
+
+        )}
       </section>
     </main>
   );
