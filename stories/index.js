46fdbaed657d11dd67769c3556180092e6000182
vlad-lab319 <solo15mag@gmail.com>
2022-01-20 00:15:43 -0500

    Add Confirm component and story for that

--- stories/index.js
+++ stories/index.js
@@ -14,6 +14,7 @@ import Appointment from "components/Appointment/index";
 import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
+import Confirm from "components/Appointment/Confirm";
 
 // Test for button component
 storiesOf("Button", module)
@@ -171,5 +172,11 @@ storiesOf("InterviewerListItem", module)
         interviewer={interviewer}
         onEdit={action("onEdit")}
         onDelete={action("onDelete")}
+      />
+    ))
+    .add("Confirm", () => (
+      <Confirm
+        onConfirm={action("onConfirm")}
+        onCancel={action("onCancel")}
       />
     ))
