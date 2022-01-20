fc804fc12f1c83ae6602a599b82ec55ff80afa90
vlad-lab319 <solo15mag@gmail.com>
2022-01-20 00:19:59 -0500

    Add Status component and story for that

--- stories/index.js
+++ stories/index.js
@@ -15,6 +15,7 @@ import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
+import Status from "components/Appointment/Status";
 
 // Test for button component
 storiesOf("Button", module)
@@ -178,5 +179,10 @@ storiesOf("InterviewerListItem", module)
       <Confirm
         onConfirm={action("onConfirm")}
         onCancel={action("onCancel")}
+      />
+    ))
+    .add("Status", () => (
+      <Status
+        message='Deleting'
       />
     ))
