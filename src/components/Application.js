edd0a793adb2ef96eafafd09cf0fcb3d2b886f06
vlad-lab319 <solo15mag@gmail.com>
2022-01-19 23:03:57 -0500

    Update components and stories with value and onChange

--- src/components/Application.js
+++ src/components/Application.js
@@ -23,6 +23,8 @@ const days = [
 ];
 
 
+
+
 export default function Application(props) {
   const [day, setDay] = useState('Monday');
   
@@ -39,8 +41,8 @@ export default function Application(props) {
         <nav className="sidebar__menu">
           <DayList
             days={days}
-            day={day}
-            setDay={setDay}
+            value={day}
+            onChange={setDay}
           />
         </nav>
         <img
