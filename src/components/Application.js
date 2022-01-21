0a9b000702b95a1405f7acd7fff5cf769bbece4a
vlad-lab319 <solo15mag@gmail.com>
2022-01-21 01:45:03 -0500

    Add fetch all data and update combined state

--- src/components/Application.js
+++ src/components/Application.js
@@ -5,6 +5,7 @@ import "components/Application.scss";
 
 import DayList from "./DayList";
 import Appointment from "./Appointment";
+import { getAppointmentsForDay } from "helpers/selectors";
 
 // const days = [
 //   {
@@ -24,50 +25,49 @@ import Appointment from "./Appointment";
 //   },
 // ];
 
-const appointments = [
-  {
-    id: 1,
-    time: "12pm",
-  },
-  {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer:{
-        id: 3,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  {
-    id: 3,
-    time: "2pm",
-  },
-  {
-    id: 4,
-    time: "3pm",
-    interview: {
-      student: "Archie Andrews",
-      interviewer:{
-        id: 4,
-        name: "Cohana Roy",
-        avatar: "https://i.imgur.com/FK8V841.jpg",
-      }
-    }
-  },
-  {
-    id: 5,
-    time: "4pm",
-  }
-];
+// const appointments = [
+//   {
+//     id: 1,
+//     time: "12pm",
+//   },
+//   {
+//     id: 2,
+//     time: "1pm",
+//     interview: {
+//       student: "Lydia Miller-Jones",
+//       interviewer:{
+//         id: 3,
+//         name: "Sylvia Palmer",
+//         avatar: "https://i.imgur.com/LpaY82x.png",
+//       }
+//     }
+//   },
+//   {
+//     id: 3,
+//     time: "2pm",
+//   },
+//   {
+//     id: 4,
+//     time: "3pm",
+//     interview: {
+//       student: "Archie Andrews",
+//       interviewer:{
+//         id: 4,
+//         name: "Cohana Roy",
+//         avatar: "https://i.imgur.com/FK8V841.jpg",
+//       }
+//     }
+//   },
+//   {
+//     id: 5,
+//     time: "4pm",
+//   }
+// ];
 
 
 
 export default function Application(props) {
   // const [day, setDay] = useState('Monday');
-
   // const [days, setDays] = useState(['test']);
   // const [appointmets, setAppointments] = useState({});
 
@@ -75,35 +75,35 @@ export default function Application(props) {
   const [state, setState] = useState ({
     day: "Monday",
     days: [],
-    appointments: {}
+    appointments: {},
+    interviewers: {}
 
   });
 
+  let dailyAppointments = [];
+
   const setDay = day => setState({ ...state, day });
-  const setDays = days => setState(prev => ({ ...prev, days }));
+  // const setDays = days => setState(prev => ({ ...prev, days }));
 
 
   useEffect(() => {
-    axios.get('/api/days')
-    // axios.get('/api/appointments')
-    .then(
-      res => {
-        console.log(res.data);
-        console.log(res.data[0].appointments);
-        setDays([...res.data]);      
-        // setDays(res.data);
-
-      }
-      )
+    Promise.all([
+      axios.get('/api/days'),
+      axios.get('/api/appointments'),
+      axios.get('/api/interviewers')
+      ]).then(all => {
+        console.log('Fetched data: ', "days", all[0].data, "app", all[1].data, "int", all[2].data);
+        setState(prev => ({...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }));
+      })
       .catch(err => console.log(err.message)
       );
     }, []);
-    
-    
+
+    dailyAppointments = getAppointmentsForDay(state, state.day);
+
     return (
       <main className="layout">
       <section className="sidebar">
-        {/* Replace this with the sidebar elements during the "Project Setup & Familiarity" activity. */}
         <img
           className="sidebar--centered"
           src="images/logo.png"
@@ -125,9 +125,8 @@ export default function Application(props) {
 
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
-        {/* {console.log('Days: ', days[0].appointments)} */}
-        {appointments.map(appointment => 
+        {/* {state.appointments.map(appointment =>  */}
+        {dailyAppointments.map(appointment => 
           <Appointment key={appointment.id} {...appointment} />
 
         )}
